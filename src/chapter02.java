e54fbd4638aefd47c61210556ec6dfaf7538c508
tajimanaoto <tajimanaoto@192.168.1.76>
2021-07-21 16:45:04 +0900

    Update code

--- src/chapter02.java
+++ src/chapter02.java
@@ -1,6 +1,7 @@
 
 public class chapter02 {
-    public static void main(String[] args) {
-        System.out.println("こんにちは！");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello WEBCAMP!");
+		System.out.println("Hello Java");
+	}
 }
