cbe9096484574d7c7b69a5d97ace433177a60ed6
tajimanaoto <tajimanaoto@192.168.1.76>
2021-07-21 18:31:20 +0900

    java Phase3

---
+++ src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "田島直人";
+		System.out.println(str1);
+		
+		str1 = "頑張る";
+		System.out.println(str1);
+		
+		int num1;
+		num1 = 24;
+		System.out.println(num1);
+	}
+
+}
